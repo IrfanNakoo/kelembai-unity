70bace3e533869dcbd3b8e27ab02148759a3ab21
IrfanNakoo <135126811+IrfanNakoo@users.noreply.github.com>
2024-06-14 17:11:29 +0800

    update 5:10 14/6/2024
    baiki input box lagi.. dh siap.. cume kene disable keyboard control spaceship..... n tambah function lain ah

--- Assets/Kelembai Item/Code/InputMechanic/InputMechanic.cs
+++ Assets/Kelembai Item/Code/InputMechanic/InputMechanic.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.InputSystem;
+using TMPro; // Include this for TextMeshPro
 
 public class InputMechanic : MonoBehaviour
 {
@@ -10,52 +10,66 @@ public class InputMechanic : MonoBehaviour
     public GameObject chatPanel, textObject;
     public InputField inputBox;
 
+    public Color playerMessage, info;
+
     [SerializeField]
     List<Message> messageList = new List<Message>();
 
-    void Start() //check barang ad ke tk jee
+    void Start()
     {
         if (chatPanel == null)
         {
-            Debug.LogError("chatPanel is not assigned in the Inspector.");
+            chatPanel = GameObject.FindWithTag("ChatPanel"); // Example: Find by tag
+            if (chatPanel == null)
+            {
+                Debug.LogError("chatPanel is not assigned in the Inspector and could not be found dynamically.");
+            }
         }
         if (textObject == null)
         {
             Debug.LogError("textObject is not assigned in the Inspector.");
         }
+        if (inputBox == null)
+        {
+            inputBox = FindObjectOfType<InputField>();
+            if (inputBox == null)
+            {
+                Debug.LogError("inputBox is not assigned in the Inspector and could not be found dynamically.");
+            }
+        }
     }
 
     void Update()
     {
-        if (inputBox.text != "")
+        if (inputBox != null && inputBox.text != "")
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                SendMessageToChat(inputBox.text);
+                SendMessageToChat(inputBox.text, Message.MessageType.playerMessage);
                 inputBox.text = "";
             }
         }
         else
         {
-            if (!inputBox.isFocused && Input.GetKeyDown(KeyCode.Return))
+            if (inputBox != null && !inputBox.isFocused && Input.GetKeyDown(KeyCode.Return))
             {
                 inputBox.ActivateInputField();
             }
         }
 
-        if (!inputBox.isFocused)
+        if (inputBox != null && !inputBox.isFocused)
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                SendMessageToChat("You pressed the enter key.");
+                SendMessageToChat("You pressed the enter key.", Message.MessageType.info);
                 Debug.Log("Enter is pressed");
             }
         }
     }
 
-    public void SendMessageToChat(string text)
+    public void SendMessageToChat(string text, Message.MessageType messageType)
     {
-        if (chatPanel == null || textObject == null) //check barang ad ke tk jee
+        if (chatPanel == null || textObject == null)
         {
             Debug.LogError("chatPanel or textObject is not assigned.");
             return;
@@ -67,8 +81,7 @@ public class InputMechanic : MonoBehaviour
             messageList.RemoveAt(0);
         }
 
-        Message newMessage = new Message();
-        newMessage.text = text;
+        Message newMessage = new Message { text = text };
 
         GameObject newText = Instantiate(textObject, chatPanel.transform);
         if (newText == null)
@@ -90,7 +103,23 @@ public class InputMechanic : MonoBehaviour
         }
 
         newMessage.textObject.text = newMessage.text;
+        newMessage.textObject.color = MessageTypeColor(messageType);
+
         messageList.Add(newMessage);
+    }
+
+    Color MessageTypeColor(Message.MessageType messageType)
+    {
+        Color color = info;
+
+        switch (messageType)
+        {
+            case Message.MessageType.playerMessage:
+                color = playerMessage;
+                break;
+        }
+
+        return color;
     }
 }
 
@@ -99,4 +128,11 @@ public class Message
 {
     public string text;
     public Text textObject;
+    public MessageType messageType;
+
+    public enum MessageType
+    {
+        playerMessage,
+        info
+    }
 }
